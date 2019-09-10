eac51f367f42e495a9062c76dd8abbd3c3a6b56d
Philip Colmer <philip.colmer@linaro.org>
2019-09-10 15:58:45 +0100

    SImplify test script further

--- emailtest.py
+++ emailtest.py
@@ -4,25 +4,16 @@
 import os
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
+import shared.globals
 import shared.email
 
-file_dir = os.path.dirname(os.path.abspath(__file__))
-with open("%s/developer_cloud_registration_email.txt" % file_dir, "r") as email_file:
-    body = email_file.read()
+shared.globals.initialise_config()
 
-name = "Nobody"
-email_address = "nobody@nowhere.org"
-uid = "nobody.nowhere"
-
-body = body.format(
-    name,
-    email_address,
-    uid
-)
+body = "This is a very simple email test.\r\n"
 
 msg = MIMEMultipart('alternative')
 msg['Subject'] = "Your Developer Cloud registration"
 msg['From'] = "it-support@linaro.org"
-msg['To'] = email_address
+msg['To'] = "nobody@nowhere.org"
 msg.attach(MIMEText(body, 'plain', 'utf-8'))
 shared.email.send_email(msg)
