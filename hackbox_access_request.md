53186e6a660242b17732a9bbd3dd2a2fc3e644d3
Philip Colmer <philip.colmer@linaro.org>
2019-09-24 18:12:21 +0100

    Add new request handler

---
+++ hackbox_access_request.md
@@ -0,0 +1,12 @@
+# About
+This handler is used when processing requests from staff for SSH access to the Hackbox2 service.
+
+# Form fields
+There are no form fields for this request.
+
+# Behaviour
+This handler supports both the `CREATE` and `COMMENT` events.
+
+When an issue is created, it applies the appropriate business logic and, if all the checks pass, the requester gets added to the LDAP group that controls SSH access for the service.
+
+The purpose of the `COMMENT` handler is to demonstrate a useful way to allow IT support staff to tell the handler to try the request again. This is useful because if something goes wrong and IT support needs to fix something, telling the handler to try again avoids having to get the original requestor to resubmit.
---
+++ hackbox_access_request.py
@@ -0,0 +1,68 @@
+""" Handler for Hackbox2 access requests. """
+
+import shared.shared_ldap as shared_ldap
+import shared.shared_sd as shared_sd
+
+CAPABILITIES = [
+    "CREATE",
+    "COMMENT"
+]
+
+SAVE_TICKET_DATA = False
+
+
+def comment(ticket_data):
+    """ Comment handler """
+    _, keyword = shared_sd.central_comment_handler([], ["help", "retry"])
+
+    if keyword == "help":
+        shared_sd.post_comment("All bot commands must be internal comments and the first "
+                               "word/phrase in the comment.\r\n\r\n"
+                               "Valid commands are:\r\n"
+                               "* retry to ask the bot to process the request again after "
+                               "problems with the request have been resolved.",
+                               False)
+    elif keyword == "retry":
+        create(ticket_data)
+
+
+def create(ticket_data):
+    """ Create handler. """
+    # There aren't any fields in the form for us to process. This
+    # is a simple case of checking that the requestor is a member of
+    # staff and then adding them to the group that controls SSH access
+    # to the system.
+    email_address = shared_sd.reporter_email_address(ticket_data)
+    account_dn = shared_ldap.find_from_email(email_address)
+    valid_account = shared_ldap.is_dn_in_group("employees", account_dn) or \
+        shared_ldap.is_dn_in_group("assignees", account_dn)
+    if not valid_account:
+        shared_sd.post_comment(
+            "You must be a Linaro employee or assignee to use the "
+            "hackbox2 service.",
+            True)
+        shared_sd.resolve_ticket(resolution_state="Won't Do")
+        return
+    if shared_ldap.is_dn_in_group("hackbox-users", account_dn):
+        shared_sd.post_comment(
+            "You appear to already have access.",
+            True)
+        shared_sd.resolve_ticket()
+        return
+    if shared_ldap.add_to_group("hackbox-users", account_dn):
+        shared_sd.post_comment(
+            "Access has been granted. Please ensure you read "
+            "https://collaborate.linaro.org/display/IKB/Hackbox2 "
+            "and associated documentation so that you fully understand "
+            "what this service is, how to use it and what the limitations "
+            "are.",
+            True)
+        shared_sd.resolve_ticket()
+    else:
+        shared_sd.post_comment(
+            "A problem occurred while adding you to the permission list. "
+            "It will be necessary to get IT Services to investigate.",
+            True)
+        # Deassign the ticket
+        shared_sd.assign_issue_to(None)
+        shared_sd.transition_request_to("Waiting for Support")
