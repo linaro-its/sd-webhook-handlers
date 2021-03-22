7d7054a7eb334dcfd98c77adb826b382914efe14
Philip Colmer <philip.colmer@linaro.org>
2021-03-22 11:16:56 +0000

    Indicate empty groups

--- owned_groups.py
+++ owned_groups.py
@@ -22,7 +22,7 @@ def create(ticket_data):
     # Need to get all of the groups, with their owners
     all_groups = shared_ldap.find_matching_objects(
         "(objectClass=groupOfUniqueNames)",
-        ["owner", "displayName", "cn"]
+        ["owner", "displayName", "cn", "uniqueMember"]
     )
     owned_groups = []
     for group in all_groups:
@@ -47,6 +47,16 @@ def create(ticket_data):
         "/3/create/129].\n\n"
     )
     for group in owned_groups:
-        response += "* %s\n" % group_name(group)
+        empty = check_if_group_has_members(group)
+        response += "* %s%s\n" % (group_name(group), empty)
     shared_sd.post_comment(response, True)
     shared_sd.resolve_ticket()
+
+def check_if_group_has_members(group):
+    """ See if this group has any members """
+    memb = group.uniqueMember.values
+    # It can't be an empty list so an empty group should
+    # just be a single empty member.
+    if memb == ['']:
+        return " (empty)"
+    return ""
